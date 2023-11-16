5890c1a590328fe08cc1bf5d24cb6f610701b353
Rippanteq7 <104727253+Rippanteq7@users.noreply.github.com>
2023-11-16 09:55:42 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const path = require('path')
+
+let listModules = {}
+if (!Object.keys(listModules).length) {
+	loadModules(path.join(__dirname, 'src'))
+}
+
+async function loadModules(folder) {
+	for (const dir of fs.readdirSync(folder)) {
+		if (dir === 'utils') continue
+		const dirname = path.join(folder, dir)
+		try {
+			const stat = fs.statSync(dirname)
+			if (stat.isDirectory()) {
+				loadModules(dirname)
+			} else if (stat.isFile() && dirname.endsWith('.js')) {
+				const moduleName = path.basename(dirname).replace('.js', '')
+				listModules[moduleName] = require(dirname)
+			}
+		} catch (e) {
+			console.log(e)
+			delete listModules[dirname]
+		} finally {
+			listModules = Object.fromEntries(
+				Object.entries(listModules).sort(([a], [b]) => a.localeCompare(b))
+			)
+		}
+	}
+}
+
+module.exports = listModules
