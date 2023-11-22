db97bcbc5a8881576d9627a4050b4b0b189b4538
Rippanteq7 <104727253+Rippanteq7@users.noreply.github.com>
2023-11-22 16:03:35 +0000

    refactor: emojipedia result

--- src/tools/emojipedia.js
+++ src/tools/emojipedia.js
@@ -1,22 +1,28 @@
+const cheerio = require('cheerio')
 const fetch = require('#utils/fetcher')
 
 module.exports = async (emoji) => {
 	const request = await fetch(`https://emojipedia.org/${emoji}`)
 	if (!request.ok) throw new Error(request.statusText)
-	
+
 	const html = await request.text()
+	const $ = cheerio.load(html)
 	const splitHtml = html.split('"vendorsAndPlatforms":')[1].split(',"alerts"')[0]
 	const emojiList = JSON.parse(splitHtml)
-	
+
 	for (const vendor of emojiList) {
 		const { description } = vendor
-		vendor.description = description.replace(/<\/?p>/g, '')
-		
+		vendor.description = $(description).text()
+
 		for (const emoji of vendor.items) {
 			const { image: { source } } = emoji
 			emoji.image.source = `https://em-content.zobj.net/${source}`
 		}
 	}
-	
-	return emojiList
+
+	return {
+		name: $('h3.text-left.mb-2').text(),
+		description: $('.flex.flex-col.gap-2 div').eq(1).text().trim(),
+		emojis: emojiList
+	}
 }
